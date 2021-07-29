14c92b251c47872d0aeeaec2e39b74a4e7ede3be
Mxmk <komuxin06@mail.ru>
2021-07-29 15:02:32 +0500

    ф

--- chemsklad/models.py
+++ chemsklad/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class React(models.Model):
     name = models.CharField(verbose_name="Название", max_length=30)
-    qual = models.CharField(verbose_name="Квалификация", max_length=20)
+    qual = models.CharField(verbose_name="Квалификация", max_length=20, null=True)
     date = models.DateField(verbose_name="Дата поступления")
-    prov = models.CharField(verbose_name="Поставщик", max_length=20)
+    prov = models.CharField(verbose_name="Поставщик", max_length=20, blank=True, null=True)
     srok = models.DateField(verbose_name="Срок годности", null=True, blank=True)
-    place = models.CharField(verbose_name="Место хранения", max_length=30)
+    place = models.CharField(verbose_name="Место хранения", max_length=30, null=True, blank=True)
     mass = models.FloatField(verbose_name="Масса, кг", null=True, blank=True)
 
     def add(self):
